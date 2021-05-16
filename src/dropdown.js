0f3ea3b2e068b46f6ae4d5c40e7046d177d6d8f2
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-05-16 19:34:01 +0200

    window onclick hide dropdowns

--- src/dropdown.js
+++ src/dropdown.js
@@ -11,17 +11,25 @@ function dropdown () {
       body.classList.add('is-open');
       input.setAttribute('placeholder', 'ATTENTION CA DEPEND Rechercher un ingrédient')
       header.children[1].setAttribute("src", "./public/logos/logo-arrow-top.svg")
-      /* fermeture*/
-      window.onclick = function hideDropdown (e) {
-        if (!e.target.classList.contains('dropdown') && !e.target.classList.contains('inputs')) {
-          input.classList.remove('input--open')
-          body.classList.remove('is-open');
-          input.setAttribute('placeholder', 'ATTENTION CA DEPEND Ingrédient')
-          header.children[1].setAttribute("src", "./public/logos/logo-arrow-bot.svg")
-        }
-      }
+      
   }))
+
+  /* fermeture*/
+  window.onclick = function hideDropdown (e) {
+    if (!e.target.classList.contains('dropdown') && !e.target.classList.contains('inputs')) {
+    const inputs = document.querySelectorAll('.inputs');
+    const dropHeader = document.querySelectorAll('.dropdown-header'),
+    dropBodys = document.querySelectorAll('.dropdown-body');
+    dropBodys.forEach((drop) => {drop.classList.remove('is-open')})
+    inputs.forEach((inp) => {
+      inp.classList.remove('input--open')
+      inp.setAttribute('placeholder', 'ATTENTION CA DEPEND Ingrédient')
+    })
+    dropBodys.forEach((drop) => {drop.children[1].setAttribute("src", "./public/logos/logo-arrow-bot.svg")})
+  }
 }
+}
+
 
 
 
