7b5b08d8d7c3d4d68325e5998eb69e3f95686271
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-05-26 22:03:41 +0200

    dropdowns fixed

--- src/dropdown.js
+++ src/dropdown.js
@@ -2,29 +2,51 @@
  * Gère le déploiement des dropdowns ainsi que leurs badges
  */
 function dropdown () {
-  const inputs = document.querySelectorAll('.inputs');
-  let A, B; // A = le placeHolder du 1er input et B le placeholder du 2nd input si on click sur un 2nd
-  /* OUVERTURE */
+  const inputs = document.querySelectorAll('.inputs'),
+  inputIngre = document.getElementById('Ingredients'),
+  inputApp = document.getElementById('Appareil'),
+  inputUst = document.getElementById('Ustensiles');
+
   inputs.forEach(inputs => inputs.addEventListener('click', (e) => {
     const target = e.currentTarget,
-      header = target.parentNode,
-      input = target.parentNode.children[0],
-      body = target.parentNode.parentNode.children[1];
-      if (A != undefined) { // si A est déjà associé à un placeholder
-        B = input.placeholder // alors B = le placeholder du clickEvent
-      } else {
-        A = input.placeholder; // Sinon c'est A qui est = au placeholder du clickEvent
-      }
-      hideDropdonw() // On ferme tous les dropdowns
-      input.classList.add('input--open');
-      setTimeout(() => {
-        body.classList.add('is-open'); 
-       }, 500);
+    header = target.parentNode,
+    input = target.parentNode.children[0],
+    body = target.parentNode.parentNode.children[1];
+    hideDropdonw()
+    input.classList.add('input--open');
+    setTimeout(() => {
+      body.classList.add('is-open'); 
+    }, 500);    
+    header.children[1].setAttribute("src", "./public/logos/logo-arrow-top.svg");
+    header.classList.remove('dropdown-header--white')
+  }))
 
-      let newPlaceHolder // variable qui va se placer dans le placeholder de l'input du dropdown déplié 
-      switch(A) {
-        case 'Ingredients': // si on a ouvert le 1er dropdown ...
-          newPlaceHolder = 'ingrédient'; // alors la var = ingérdient
+
+  /*  gestion du input*/ 
+  inputIngre.addEventListener('click', (e) => {
+    const target = e.currentTarget,
+        input = target.parentNode.children[0];
+        inputDisplay(input)
+  })
+
+  inputApp.addEventListener('click', (e) => {
+    const target = e.currentTarget,
+        input = target.parentNode.children[0];
+        inputDisplay(input)
+  })
+
+  inputUst.addEventListener('click', (e) => {
+    const target = e.currentTarget,
+        input = target.parentNode.children[0];
+        inputDisplay(input)
+  })
+  hideDropdonw() 
+  function inputDisplay(input) {
+    let placeholder = input.placeholder
+    let newPlaceHolder
+      switch(placeholder) {
+        case 'Ingredients':
+          newPlaceHolder = 'ingrédient';
           break;
         case 'Appareil':
           newPlaceHolder = 'appareil';
@@ -35,77 +57,30 @@ function dropdown () {
         default:
           console.log(`Erreur dans le switch`);       
       }
-      if (input.placeholder.indexOf('Recherche') == -1) { // On boucle sur TOUS les inputs ; Celui qui a le mot Recherche ...
-        console.log(input.placeholder)
-      input.setAttribute('placeholder', `Rechercher un ${newPlaceHolder}`); ///
-      }
-      header.children[1].setAttribute("src", "./public/logos/logo-arrow-top.svg");
-      header.classList.remove('dropdown-header--white')
-  }))
-
+    input.setAttribute('placeholder', `Rechercher un ${newPlaceHolder}`);
+    
+  }
 
   /* FERMETURE */
-  function hideDropdonw() { 
+  function hideDropdonw() {
     inputs.forEach(input => {
       input.parentNode.parentNode.children[1].classList.remove('is-open');
       input.classList.remove('input--open');
-      if (B != undefined) {
-        if (!input.placeholder.indexOf('Rechercher')) {
-          input.setAttribute('placeholder', A);
-          A = undefined
-        } else {
-          input.setAttribute('placeholder', B);
-          B = undefined
-        }
-      } else {
-        if (!input.placeholder.indexOf('Rechercher')) {
-          input.setAttribute('placeholder', A);
-          A = undefined
-        }
-      }
       input.parentNode.children[1].setAttribute("src", "./public/logos/logo-arrow-bot.svg");
       input.parentNode.classList.add('dropdown-header--white');
     })
-   }
+    inputIngre.setAttribute('placeholder', 'Ingredients');
+    inputApp.setAttribute('placeholder', 'Appareil');
+    inputUst.setAttribute('placeholder', 'Ustensiles');
+  }
 
    window.onclick = function(e) {
      if (!e.path[0].classList.contains('inputs')) {
       hideDropdonw()
     }
    }
-
-   /* affichages des badges */
+   /* TODO affichages des badges */
 
 }
 
-
-
-
-
 export {dropdown}
-
-
-
-
-/*
-
-
-   fermeture
-  let input;
-  window.onclick = hideDropdonw;
-  
-  let hideDropdonw = function () {
-    if (e.path[0].classList.contains('inputs')) {
-      return input = e.target.parentNode.children[0];
-    }
-    if (!e.target.classList.contains('dropdown') && !e.target.classList.contains('inputs')) {
-    input.parentNode.parentNode.children[1].classList.remove('is-open');
-    input.classList.remove('input--open');
-    input.setAttribute('placeholder', inputPlaceholder);
-    console.log(input)
-    input.parentNode.children[1].setAttribute("src", "./public/logos/logo-arrow-bot.svg");
-    input.parentNode.classList.add('dropdown-header--white');
-    }
-  }
-
-*/
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { dropdown } from "./dropdown"
+import {search} from "./search"
+import {recipes} from "./recipes"
 dropdown()
+search(recipes)
 
 //TODO fonction recherche basique JS pour ressortir tous les éléments du fichiers JSON
-
