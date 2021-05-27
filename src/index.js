6ec970ff7e574fa1eab8e553ef6cfa9ff2147916
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-05-27 18:16:26 +0200

    dropdowns.js more elegant and commented

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import { dropdown } from "./dropdown"
+import { dropdown } from "./dropdowns"
 import {search} from "./search"
 import {recipes} from "./recipes"
 dropdown()
 search(recipes)
 
-//TODO fonction recherche basique JS pour ressortir tous les éléments du fichiers JSON
+// TODO fonction recherche basique JS pour ressortir tous les éléments du fichiers JSON
+
+// TODO Affichage du badge de recherche 
