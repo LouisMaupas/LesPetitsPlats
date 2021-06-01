35c62a0ef44388e9b14ac9e0d3e5a2ec39afd113
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-06-01 10:27:57 +0200

    testing basic research

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import {recipes} from "./recipes"
 import { dropdown } from "./dropdowns"
 import {search} from "./search"
-import {recipes} from "./recipes"
 dropdown()
 search(recipes)
 
 // TODO fonction recherche basique JS pour ressortir tous les éléments du fichiers JSON
 
 // TODO Affichage du badge de recherche 
+
+// TODO rendre rapprot de bug console.log lisible
