b808589b0ed4b4a743256ebf9127232991c85262
LouisMaupas <louismaupas@gmail.com>
2021-08-29 13:32:28 +0200

    closefilters bug fixed

--- src/search.js
+++ src/search.js
@@ -116,6 +116,7 @@ function search(recipes) {
 	 * @param {*} request string user input
 	 */
 	function mainSearch(request) {
+		recipesToDisplay = recipes
 		filterRecipes()
 		let goodRecipes = []
 			// titre
@@ -276,7 +277,6 @@ function search(recipes) {
 		 * Filter recipes according to the selected filters + call displayRecipes()
 		 */
 		function filterRecipes() {
-			recipesToDisplay = recipes;
 			// Create an array containing all filters (keywords) as name / type objects
 			myItemsFiltered = [];
 			// recipesToDisplay = recipes;
@@ -376,8 +376,6 @@ function search(recipes) {
 		target.remove();
 		manageKeywords();
 		mainSearch(mainSearchInput.value)
-		displayRecipes();
-		filterKeywords();
 	}
 
 	/**
